084ffcbbe4d621463624586bf6a6624469cc545f
KorJuk69 <korjuk1@gmail.com>
2017-09-26 18:00:33 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,29 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        int n = scanner.nextInt();
+        int massiv[] = new int[n];
 
+
+        for (int i=0;i<n;i++){
+            massiv[i] = scanner.nextInt();
+        }
+
+        for (int i=0;i<n;i++){
+            if (i!=0){
+                if((massiv[i]>0)&&(massiv[i-1]>0)) {
+                    System.out.print("Yes");
+                    break;
+                }
+                else if ((massiv[i]<0)&&(massiv[i-1]<0)) {
+                    System.out.print("Yes");
+                    break;
+                }
+                else {
+                    System.out.print("NO");
+                    break;
+                }
+            }
+        }
     }
 }
