09f555b3832843f9fd4b2236dcbf23942579be8e
KorJuk69 <korjuk1@gmail.com>
2017-09-26 14:38:09 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,16 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        int n = scanner.nextInt();
+        int massiv[];
+        massiv = new int[n];
 
+        for (int i=0;i<n;i++){
+            massiv[i] = scanner.nextInt();
+        }
+
+        for (int i=0;i<n;i+=2){
+            System.out.print(massiv[i] + " ");
+        }
     }
 }
