bfde7b65f7bec63bda09529c780a87391f3d1923
KorJuk69 <korjuk1@gmail.com>
2017-09-26 17:52:35 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,25 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        int n = scanner.nextInt();
+        int massiv[] = new int[n];
+        int kolvo = 0;
+        int maxkolvo = 0;
+
+        for (int i=0;i<n;i++){
+
+            massiv[i] = scanner.nextInt();
+
+            if(i!=0){
+                if(massiv[i]>massiv[i-1]){
+                    kolvo++;
+                    if (kolvo>maxkolvo) maxkolvo = kolvo;
+                }
+                else kolvo = 0;
+            }
+        }
+
+        System.out.print(maxkolvo);
 
     }
 }
